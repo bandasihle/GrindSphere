2eb338d8629e810c4c05c4fdf533d93b03c4776e
bandasihle <bandasihle710@gmail.com>
2025-04-15 16:14:11 +0200

    database

--- backend/server.js
+++ backend/server.js
@@ -35,3 +35,16 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+app.get('/test-db', (req, res) => {
+    db.query('SELECT 1 + 1 AS solution', (err, results) => {
+        if (err) {
+            console.error('Database connection error:', err);
+            res.status(500).json({ message: 'Database connection error' });
+        } else {
+            console.log('Database query result:', results);
+            res.json({ message: 'Database query successful', results });
+        }
+    });
+});
+
+
